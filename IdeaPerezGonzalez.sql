dfb126296eeb8065fffb854869c44561c72ff6f1
carlos8788 <100473510+carlos8788@users.noreply.github.com>
2022-12-20 21:47:33 -0300

    Update IdeaPerezGonzalez.sql
    Campos modificados de la tabla vuelo

--- IdeaPerezGonzalez.sql
+++ IdeaPerezGonzalez.sql
@@ -19,7 +19,7 @@ CREATE TABLE piloto (
 CREATE TABLE avion (
     id_avion INT NOT NULL AUTO_INCREMENT,
     capacidad INT NOT NULL,
-    modelo VARCHAR(45) NOT NULL,
+    modelo VARCHAR(150) NOT NULL,
     anio_creacion INT NOT NULL,
     PRIMARY KEY (id_avion)
 );
@@ -28,13 +28,13 @@ CREATE TABLE ruta (
     id_ruta INT NOT NULL AUTO_INCREMENT,
     origen VARCHAR(45) NOT NULL,
     destino VARCHAR(45) NOT NULL,
-    distancia REAL NOT NULL,
+    distancia DECIMAL(8,3) NOT NULL,
     PRIMARY KEY (id_ruta)
 );
 
 CREATE TABLE pasaje (
     id_pasaje INT NOT NULL AUTO_INCREMENT,
-    asiento VARCHAR(5) NOT NULL,
+    asiento INT NOT NULL,
     id_pasajero INT NOT NULL,
     id_vuelo INT NOT NULL,
     PRIMARY KEY (id_pasaje),
@@ -47,6 +47,7 @@ CREATE TABLE vuelo (
     id_piloto INT NOT NULL,
     id_ruta INT NOT NULL,
     id_avion INT NOT NULL,
+    fecha DATE NOT NULL,
     PRIMARY KEY (id_vuelo),
     FOREIGN KEY (id_piloto) REFERENCES piloto(id_piloto),
     FOREIGN KEY (id_ruta) REFERENCES ruta(id_ruta),
